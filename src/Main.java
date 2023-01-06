a5c579de91ae7cbef484257dbe5626d34d99ab1a
monstro-montra <70246029+monstro-montra@users.noreply.github.com>
2023-01-06 00:37:03 -0500

    added first compliant example. both are now commented out.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 public class Main {
-    //Non-compliant example: variable declaration outside of loop.
-    public static void main(String[] args) {
-        int i = 0; //variable declared outside of loop when it should be declared within loop.
-        for (i = 0; i < 5; i++ ){
-            System.out.println("Hello world!");
-        }
-    }
+
+
+
 }
+    //Compliant example: variable declared within for statement.
+//    public static void main(String[] args) {
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Hello world!");
+//        }
+//    }
+//}
+
+
+    //Non-compliant example: variable declaration outside of loop.
+//    public static void main(String[] args) {
+//        int i = 0; //variable declared outside of loop when it should be declared within loop.
+//        for (i = 0; i < 5; i++ ){
+//            System.out.println("Hello world!");
+//        }
+//    }
+//}
